e0c1318a5c8798b1992d1df4c8c09f3a3e801a6c
stars198484 <stars198484@mail.ru>
2017-06-21 21:14:18 +0500

    Создан проект

---
+++ individual_minute/app/src/main/java/ru/geekbrains/android1/stas/individual_minute/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.android1.stas.individual_minute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
