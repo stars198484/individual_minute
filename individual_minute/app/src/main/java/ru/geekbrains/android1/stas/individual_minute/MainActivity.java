552528292f173ec12e2c6b601de1641b56c63d54
stars198484 <stars198484@mail.ru>
2017-06-21 22:38:17 +0500

    создан майн

--- individual_minute/app/src/main/java/ru/geekbrains/android1/stas/individual_minute/MainActivity.java
+++ individual_minute/app/src/main/java/ru/geekbrains/android1/stas/individual_minute/MainActivity.java
@@ -1,13 +1,27 @@
 package ru.geekbrains.android1.stas.individual_minute;
 
+import android.net.sip.SipAudioCall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnStart;
+    Button btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = (Button)findViewById(R.id.btStart);
+        btnStop = (Button)findViewById(R.id.btStop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
