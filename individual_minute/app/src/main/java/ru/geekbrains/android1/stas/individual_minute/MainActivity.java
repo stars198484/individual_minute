21321fbe81f3fddf15e855903e8dc10bd30e3fcd
stars198484 <stars198484@mail.ru>
2017-07-05 23:48:38 +0500

    почти все, месяц не правильно отображает

--- individual_minute/app/src/main/java/ru/geekbrains/android1/stas/individual_minute/MainActivity.java
+++ individual_minute/app/src/main/java/ru/geekbrains/android1/stas/individual_minute/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.android1.stas.individual_minute;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.sip.SipAudioCall;
@@ -13,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -45,21 +48,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         db = new DB(this);
         db.open();
-        String[] from = new String[]{DB.COLUMN_DATE, DB.COLUMN_TIME};
         btnStart = (Button) findViewById(R.id.btStart);
         btnStop = (Button) findViewById(R.id.btStop);
         tvInfo = (TextView) findViewById(R.id.tvInfo);
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
-        int[] to = new int[]{R.id.txtDate, R.id.txtTime};
-        scAdapter = new SimpleCursorAdapter(this, R.layout.item, null, from, to, 0);
         lvData = (ListView) findViewById(R.id.lvData);
-        lvData.setAdapter(scAdapter);
         lvData.setOnItemClickListener(this);
         registerForContextMenu(lvData);
-        getSupportLoaderManager().initLoader(0, null, this);
         btnStop.setEnabled(false);
         tvInfo.setText(getString(R.string.info_ready));
+    }
+
+    public void showData(String date, String time) {
+        String[] from = new String[]{date, time};
+        int[] to = new int[]{R.id.txtDate, R.id.txtTime};
+        scAdapter = new SimpleCursorAdapter(this, R.layout.item, null, from, to, 0);
+        lvData.setAdapter(scAdapter);
+        getSupportLoaderManager().initLoader(0, null, this);
+        getSupportLoaderManager().getLoader(0).forceLoad();
     }
 
     @Override
@@ -76,9 +83,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Calendar now = Calendar.getInstance(TimeZone.getDefault());
                 int year = now.get(Calendar.YEAR);
                 int month = now.get(Calendar.MONTH);
+                int day = now.get(Calendar.DAY_OF_MONTH);
                 int hour = now.get(Calendar.HOUR_OF_DAY);
                 int minute = now.get(Calendar.MINUTE);
-                dateNow = String.valueOf(month) + '.' + String.valueOf(year) + ' ' + String.valueOf(hour) + ':' + String.valueOf(minute);
+                dateNow = String.valueOf(day) + '.' +String.valueOf(month) + '.' + String.valueOf(year) + ' ' + String.valueOf(hour) + ':' + String.valueOf(minute);
 //                Toast toast = Toast.makeText(getApplicationContext(),
 //                        dateNow + ' ' + startMillis, Toast.LENGTH_LONG);
 //                toast.show();
@@ -90,13 +98,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 stopMillis = date.getTime();
                 time = String.valueOf(((stopMillis - startMillis) / 1000));
                 db.addRec(dateNow, time);
-//                Toast toast = Toast.makeText(getApplicationContext(),
-//                       time+ ' ' + stopMillis, Toast.LENGTH_LONG);
-//                toast.show();
+                Toast toast = Toast.makeText(getApplicationContext(),
+                       getString(R.string.end), Toast.LENGTH_LONG);
+                toast.show();
                 btnStop.setEnabled(false);
                 break;
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_clear_db) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(getString(R.string.q_menu_clear_db));
+            builder.setCancelable(false);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    db.clearDB();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        if (item.getItemId() == R.id.menu_clear_list) {
+           lvData.setAdapter(null);
+        }
+        if (item.getItemId() == R.id.menu_result) {
+            showData(DB.COLUMN_DATE, DB.COLUMN_TIME);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onCreateContextMenu(ContextMenu menu, View v,
@@ -151,7 +193,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public Cursor loadInBackground() {
-            return null;
+            return db.getAllData();
         }
     }
 }
